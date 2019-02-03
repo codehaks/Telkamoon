cc3588f57762a8a8e68af101d5bf35009a457337
Hakim Ghods <ghods.hakim@hotmail.com>
2019-02-03 17:38:23 +0330

    UserList

---
+++ src/Portal.Web/ViewComponents/UserListViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Portal.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Web.ViewComponents
+{
+    [ViewComponent]
+    public class UserListViewComponent:ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserListViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+
+        }
+    }
+}
