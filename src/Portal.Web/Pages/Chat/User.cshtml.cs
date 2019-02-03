86d2a607b17ab3f6876f6c829a8c20e3c702f830
Hakim Ghods <ghods.hakim@hotmail.com>
2019-02-03 19:48:39 +0330

    CHat

---
+++ src/Portal.Web/Pages/Chat/User.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portal.Web.Pages.Chat
+{
+    public class UserModel : PageModel
+    {
+        public string UserName { get; set; }
+        public void OnGet(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
