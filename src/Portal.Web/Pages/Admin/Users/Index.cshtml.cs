beffece47c0e1c3c9928b99d7e02117c522e7f48
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-10 14:17:25 +0430

    Fix

--- src/Portal.Web/Pages/Admin/Users/Index.cshtml.cs
+++ src/Portal.Web/Pages/Admin/Users/Index.cshtml.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portal.Domain.Models;
 
 namespace Portal.Web.Pages.Admin.Users
 {
     public class IndexModel : PageModel
     {
-        public IList<IdentityUser> Output { get; set; }
-        public void OnGet([FromServices] UserManager<IdentityUser> userManager)
+        public IList<ApplicationUser> Output { get; set; }
+        public void OnGet([FromServices] UserManager<ApplicationUser> userManager)
         {
             Output = userManager.Users.ToList();
         }
